09c94bf69a116f8d53d1d760ecbf0698319feab4
jake <gerion@gmail.com>
2023-12-19 19:18:51 +0900

    쓰레드풀 기본 구현체 추가

--- src/test/java/com/jake/threadpool/ThreadPoolTest.java
+++ src/test/java/com/jake/threadpool/ThreadPoolTest.java
@@ -1,2 +1,21 @@
-package com.jake.threadpool;public class ThreadPoolTest {
+package com.jake.threadpool;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executor;
+
+class ThreadPoolTest {
+
+     @Test
+     void submittedTasksAreExecuted() {
+         final Executor executor = new ThreadPool(2);
+
+         for(int i=0; i < 10; i++) {
+             final int finalI = i;
+             executor.execute(() -> {
+                 System.out.println("Thread '" + Thread.currentThread().getName() + "' executes a task " + finalI);
+
+             });
+         }
+     }
 }
