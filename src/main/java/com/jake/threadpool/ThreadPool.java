f80d28c5b93a19ffc8080b9dadbad7240d7b759b
jake <gerion@gmail.com>
2023-12-25 22:27:42 +0900

    Thread lazy init 형태로 개선

--- src/main/java/com/jake/threadpool/ThreadPool.java
+++ src/main/java/com/jake/threadpool/ThreadPool.java
@@ -1,35 +1,68 @@
 package com.jake.threadpool;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadPool implements Executor {
 
+    private static final Thread[] EMPTY_THREADS_ARRAY = new Thread[0];
     private static final Runnable SHUTDOWN_TASK = () -> {};
-
+    private final int maxNumThreads;
     private final BlockingQueue<Runnable> queue = new LinkedTransferQueue<>();
-
-    private final Thread[] threads;
-
-    private final AtomicBoolean started = new AtomicBoolean();
-
     private final AtomicBoolean shutdown = new AtomicBoolean();
+    private final Set<Thread> threads = new HashSet<>();
+    private final Lock threadsLock = new ReentrantLock();
+    private final AtomicInteger numThreads = new AtomicInteger();
+    private final AtomicInteger numActiveThreads = new AtomicInteger();
 
-    public ThreadPool(int numThreads) {
-        this.threads = new Thread[numThreads];
-        for(int i=0; i < numThreads; i++) {
-            threads[i] = new Thread(() -> {
-//                while(!shutdown || !queue.isEmpty()) {
+    public ThreadPool(int maxNumThreads) {
+        this.maxNumThreads = maxNumThreads;
+    }
+
+    private Thread newThread() {
+
+        numThreads.incrementAndGet();
+        numActiveThreads.incrementAndGet();
+
+        final Thread thread = new Thread(() -> {
+            boolean isActive = true;
+            try {
                 for (;;) {
                     try {
-                        final Runnable task = queue.take();
+                        Runnable task = queue.poll();
+                        if (task == null) {
+                            if( isActive ) {
+                                isActive = false;
+                                numActiveThreads.decrementAndGet();
+                            }
+                            task = queue.take();
+                            isActive = true;
+                            numActiveThreads.incrementAndGet();
+                        } else {
+                            if(!isActive) {
+                                isActive = true;
+                                numActiveThreads.incrementAndGet();
+                            }
+                        }
+
+//                        final Runnable task = queue.take();
                         if(task == SHUTDOWN_TASK) {
                             break;
                         } else {
-                            task.run();
+                            numActiveThreads.incrementAndGet();
+                            try {
+                                task.run();
+                            } finally {
+                                numActiveThreads.decrementAndGet();
+                            }
                         }
                     } catch(Throwable t) {
                         if(!(t instanceof InterruptedException)) {
@@ -38,45 +71,97 @@ public class ThreadPool implements Executor {
                         }
                     }
                 }
+            } finally {
+                threadsLock.lock();
+                try {
+                    threads.remove(Thread.currentThread());
+                } finally {
+                    threadsLock.unlock();
+                }
+                numThreads.decrementAndGet();
+                if (isActive) {
+                    numActiveThreads.decrementAndGet();
+                }
+            }
+            System.err.println("Shutting down thread '" + Thread.currentThread().getName() + '\'');
 
-                System.err.println("Shutting thread '" + Thread.currentThread().getName() + '\'');
+        });
+        threads.add(thread);
 
-            });
-        }
+        return thread;
     }
 
     @Override
     public void execute(Runnable command) {
-        if(started.compareAndSet(false, true)) {
-            for (Thread thread: threads) {
-                thread.start();
-            }
-        }
-
         if(shutdown.get()) {
             throw new RejectedExecutionException();
         }
+
         queue.add(command);
+        addThreadIfNecessary();
+
+
+        if(shutdown.get()) {
+            queue.remove(command);
+            throw new RejectedExecutionException();
+        }
+    }
+
+    private void addThreadIfNecessary() {
+        if(needsMoreThreads()) {
+            threadsLock.lock();
+            Thread newThread = null;
+            try {
+                if(needsMoreThreads()) {
+                    newThread = newThread();
+                }
+            } finally {
+                threadsLock.unlock();
+            }
+            if (newThread != null) {
+                newThread.start();
+            }
+        }
+    }
+
+    private boolean needsMoreThreads() {
+        final int numActiveThreads = this.numActiveThreads.get();
+        final int numThreads = this.numThreads.get();
+        return numActiveThreads < maxNumThreads && numActiveThreads >= numThreads;
     }
 
     public void shutdown() {
 
         if (shutdown.compareAndSet(false, true)) {
             // wait until the queue is completely drained.
-            for (int i=0; i < threads.length; i++) {
+            for (int i=0; i < maxNumThreads; i++) {
                 queue.add(SHUTDOWN_TASK);
             }
         }
 
         //
-        for(Thread thread : threads) {
-            do {
-                try {
-                    thread.join();
-                } catch (InterruptedException e) {
-                    // Do not propagate to prevent incomplete shutdown.
-                }
-            } while (thread.isAlive());
+        for (;;) {
+            final Thread[] threads;
+            threadsLock.lock();
+            try {
+                threads = this.threads.toArray(EMPTY_THREADS_ARRAY);
+            } finally {
+                threadsLock.unlock();
+            }
+
+            if(threads.length == 0) {
+                break;
+            }
+
+            for(Thread thread : threads) {
+                do {
+                    try {
+                        thread.join();
+                    } catch (InterruptedException e) {
+                        // Do not propagate to prevent incomplete shutdown.
+                    }
+                } while (thread.isAlive());
+            }
         }
     }
 }
--- src/test/java/com/jake/threadpool/ThreadPoolTest.java
+++ src/test/java/com/jake/threadpool/ThreadPoolTest.java
@@ -2,20 +2,32 @@ package com.jake.threadpool;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 
 class ThreadPoolTest {
 
      @Test
      void submittedTasksAreExecuted() {
-         final Executor executor = new ThreadPool(2);
-
-         for(int i=0; i < 100; i++) {
-             final int finalI = i;
-             executor.execute(() -> {
-                 System.err.println("Thread '" + Thread.currentThread().getName() + "' executes a task " + finalI);
-
-             });
+         final ThreadPool executor = new ThreadPool(100);
+         final int numTasks = 100;
+         final CountDownLatch latch = new CountDownLatch(numTasks);
+         try {
+             for(int i=0; i < 100; i++) {
+                 final int finalI = i;
+                 executor.execute(() -> {
+                     System.err.println("Thread '" + Thread.currentThread().getName() + "' executes a task " + finalI);
+                     try {
+                         Thread.sleep(10);
+                     } catch (InterruptedException e) {
+                         //
+                     }
+                     latch.countDown();
+                 });
+             }
+         } finally {
+             executor.shutdown();
          }
+
      }
 }
